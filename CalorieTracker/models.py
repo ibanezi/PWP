3cffcbcb3db38b3b7036c2a4cd15a29b28c9c3d5
Niklas Riikonen <niriikon@gmail.com>
2021-02-21 21:34:26 +0200

    Populate database with setup.py, set foreign key deletion behavior for models.

--- CalorieTracker/models.py
+++ CalorieTracker/models.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -11,16 +11,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(16), nullable=False, unique=True)
 
-    meals = db.relationship("Meal", back_populates="user")
+    meals = db.relationship('Meal', back_populates='user', cascade='all, delete')
 
 class Meal(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	userId = db.Column(db.Integer, db.ForeignKey("user.id"))
+	userId = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
 	name = db.Column(db.String(50), nullable=False)
 	description = db.Column(db.String(255))
 
-	user = db.relationship("User", back_populates="meals")
-	ingredients = db.relationship("IngredientsInMeal", back_populates="meal")
+	user = db.relationship('User', back_populates='meals')
+	ingredients = db.relationship('IngredientsInMeal', back_populates='meal', cascade='all, delete')
 
 class Ingredient(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -31,13 +31,13 @@ class Ingredient(db.Model):
 	proteins = db.Column(db.Float)
 	fats = db.Column(db.Float)
 
-	meal = db.relationship("IngredientsInMeal", back_populates="ingredients")
+	meal = db.relationship('IngredientsInMeal', back_populates='ingredients', cascade='all, delete')
 
 class IngredientsInMeal(db.Model):
-	mealId = db.Column(db.Integer, db.ForeignKey("meal.id"), primary_key=True)
-	ingredientId = db.Column(db.Integer, db.ForeignKey("ingredient.id"), primary_key=True)
+	mealId = db.Column(db.Integer, db.ForeignKey('meal.id'), primary_key=True)
+	ingredientId = db.Column(db.Integer, db.ForeignKey('ingredient.id'), primary_key=True)
 	ssize = db.Column(db.Float, nullable=False)
 
-	meal = db.relationship("Meal", back_populates="ingredients")
-	ingredients = db.relationship("Ingredient", back_populates="meal")
+	meal = db.relationship('Meal', back_populates='ingredients')
+	ingredients = db.relationship('Ingredient', back_populates='meal')
 
--- setup.py
+++ setup.py
@@ -1,3 +1,120 @@
-# Placeholder setup.py for structure, please replace with actual setup file
-from CalorieTracker.models import db
+from os import path, remove
+from CalorieTracker.models import *
+
+DB_PATH = './CalorieTracker/test.db'
+
+# Delete database because we are populating with values that have uniqueness constraint (user.name)
+if path.isfile(DB_PATH):
+    print('Old testing database exists, deleting...')
+    remove(DB_PATH)
+
 db.create_all()
+
+# Populate database with test data
+users = [User(name='TestUser1'), User(name='TestUser2'), User(name='TestUser3'), User(name='TestUser4')]
+for u in users:
+    db.session.add(u)
+db.session.commit()
+
+ingreds = [Ingredient(
+        name='Tomato',
+        ssize=100,
+        kcal=23,
+        carbs=62,
+        proteins=10,
+        fats=12
+        ),
+    Ingredient(
+        name='Potato',
+        ssize=150,
+        kcal=112,
+        carbs=125,
+        proteins=15,
+        fats=2
+        ),
+    Ingredient(
+        name='Beer',
+        ssize=330,
+        kcal=142,
+        carbs=128,
+        proteins=13,
+        fats=0
+        ),
+    Ingredient(
+        name='Ground beef',
+        ssize=400,
+        kcal=648,
+        carbs=0,
+        proteins=208,
+        fats=192
+        ),
+    Ingredient(
+        name='Egg',
+        ssize=60,
+        kcal=80,
+        carbs=0,
+        proteins=23,
+        fats=36
+        ),
+    Ingredient(
+        name='Lazy potato',
+        ssize=150,
+        kcal=110
+        )]
+
+for i in ingreds:
+    db.session.add(i)
+db.session.commit()
+
+meals = [
+    Meal(
+        userId=3,
+        name='Breakfast',
+        description='Sunday morning breakfast'
+        ),
+    Meal(
+        userId=1,
+        name='Dinner'
+        ),
+    Meal(
+        userId=2,
+        name='Breakfast',
+        description='Omelette with tomatoes'
+        )
+    ]
+
+for m in meals:
+    db.session.add(m)
+db.session.commit()
+
+meal_items = [
+    IngredientsInMeal(
+        mealId=1,
+        ingredientId=3,
+        ssize=6
+        ),
+    IngredientsInMeal(
+        mealId=2,
+        ingredientId=2,
+        ssize=3
+        ),
+    IngredientsInMeal(
+        mealId=2,
+        ingredientId=3,
+        ssize=0.3
+        ),
+    IngredientsInMeal(
+        mealId=3,
+        ingredientId=5,
+        ssize=2
+        ),
+    IngredientsInMeal(
+        mealId=3,
+        ingredientId=1,
+        ssize=3
+        )
+    ]
+
+for mi in meal_items:
+    db.session.add(mi)
+db.session.commit()
